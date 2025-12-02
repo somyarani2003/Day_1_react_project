e9cb9b9d92080c74f4e04442226acd4247200da8
Ankita Kumari <ankita.ac.dv@gmail.com>
2025-12-02 12:57:17 +0530

    Initial commit

---
+++ project-1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HeroSection from './components/Hero';
+import Navigation from './components/Navigation';
+
+const App = () =>{
+    return(
+     <>
+       <Navigation/>
+       <HeroSection/>
+    </>
+    ) 
+}
+export default App;
